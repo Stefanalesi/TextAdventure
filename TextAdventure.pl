8047914afc14699c9bcfff8fe4932c4fb6d736c7
HasenschwandtnerAndreas <113516687+HasenschwandtnerAndreas@users.noreply.github.com>
2024-06-21 18:53:26 +0200

    fixed many problems

--- TextAdventure.pl
+++ TextAdventure.pl
@@ -16,17 +16,16 @@ path(corridor1, e, corridor2).
 path(corridor2, w, corridor1).
 
 closed(bedroomdoor).
-path(corridor1, s, bedroom) :- closed(bedroomdoor), write('Die Tür ist verschlossen. Finde einen Weg, um sie zu öffnen.'), fail.
+path(corridor1, s, bedroom) :- closed(bedroomdoor), write('Die Tuer ist verschlossen. Finde einen Weg, um sie zu oeffnen.'), fail.
 path(corridor1, s, bedroom) :- \+ closed(bedroomdoor).
 path(bedroom, n, corridor1).
 
 closed(schrank).
 path(bedroom, w, schrank).
-path(bedroom, e, bedroom).
+path(schrank, e, bedroom).
 
 closed(vent).
-path(bedroom, e, vent) :- closed(vent), write('Der Lüftungsschacht ist verschlossen. Finde einen Weg, um ihn zu öffnen.'), fail.
-path(bedroom, e, vent) :- \+ closed(vent).
+path(bedroom, e, vent).
 path(vent, w, bedroom).
 
 path(corridor2, n, storage).
@@ -37,7 +36,8 @@ path(storage, e, wardrobe).
 path(wardrobe, w, storage).
 
 at(schere, home).
-at(schraubenschlüssel, home).
+at(schraubenschluessel, home).
+at(dietrich, home).
 at(bueroklammer, tv).
 at(taschenmesser, storage).
 
@@ -45,17 +45,18 @@ at(taschenmesser, storage).
 use :- i_am_at(Place), useable_at(Place).
 use :- i_am_at(Place), \+ useable_at(Place), write('Du kannst hier nichts benutzen.').
 
-useable_at(vent) :- holding(schraubenschlüssel), assert(holding(funkgeraet)), write('Du hast den Lüftungsschacht geöffnet und ein Funkgerät gefunden, welches jedoch keine Batterie hat.'), nl.
-useable_at(vent) :- \+ holding(schraubenschlüssel), write('Du brauchst einen Schraubenschlüssel, um den Lüftungsschacht zu öffnen.'), nl.
+useable_at(vent) :- closed(vent), holding(schraubenschluessel), assert(holding(funkgeraet)), retract(closed(vent)), write('Du hast den Lueftungsschacht geoeffnet und ein Funkgeraet gefunden, welches jedoch keine Batterie hat.'), nl.
+useable_at(vent) :- \+ closed(vent), write('Der Lüftungsschacht ist bereits offen und du hast alles genommen.'), nl.
+useable_at(vent) :- \+ holding(schraubenschluessel), write('Du brauchst einen Schraubenschluessel, um den Lueftungsschacht zu oeffnen.'), nl.
 
-useable_at(schrank) :- retract(closed(bedroomdoor)), assert(holding(batterie)), write('Du hast eine Batterie gefunden.'), nl.
+useable_at(schrank) :- retract(closed(schrank)), assert(holding(batterie)), write('Du hast eine Batterie gefunden.'), nl.
 useable_at(schrank) :- \+ closed(schrank), write('Du hast den Schrank bereits durchsucht.'), nl.
 
-useable_at(corridor1) :- holding(dietrich), retract(closed(bedroomdoor)), write('Du hast die Tür geöffnet.'), nl.
-useable_at(corridor1) :- \+ holding(dietrich), write('Du brauchst einen Schlüssel, um die Tür zu öffnen, oder du findest einen anderen Weg :). Aber leider hast du es nicht rechtzeitig geschafft, den Schlüssel mitzunehmen.'), nl.
+useable_at(corridor1) :- holding(dietrich), retract(closed(bedroomdoor)), write('Du hast die Tuer geoeffnet.'), nl.
+useable_at(corridor1) :- \+ holding(dietrich), write('Du brauchst einen Schluessel, um die Tuer zu oeffnen, oder du findest einen anderen Weg :). Aber leider hast du es nicht rechtzeitig geschafft, den Schluessel mitzunehmen.'), nl.
 
 useable_at(workbench) :- holding(bueroklammer), holding(taschenmesser), retract(holding(bueroklammer)), retract(holding(taschenmesser)), assert(holding(dietrich)), write('Du hast dir einen Dietrich gebaut. Vielleicht kannst du ihn ja einsetzen.'), nl.
-useable_at(workbench) :- holding(funkgeraet), holding(batterie), retract(holding(batterie)), write('Glückwunsch, du hast es geschafft! Mit dem Funkgerät hast du das Militär kontaktiert, und sie haben dir geholfen, aus dem Bunker zu kommen.'), nl.
+useable_at(workbench) :- holding(funkgeraet), holding(batterie), retract(holding(batterie)), write('Glueckwunsch, du hast es geschafft! Du hast die Batterie in das Funkgeraet gelegt und mit dem Funkgeraet hast du das Militaer kontaktiert, und sie haben dir geholfen, aus dem Bunker zu kommen.'), nl.
 useable_at(workbench) :- write('Du kannst gerade nichts craften.'), nl.
 
 /* These rules describe how to pick up an object. */
@@ -129,7 +130,7 @@ die :-
 /* Under UNIX, the "halt." command quits Prolog but does not remove the output window. On a PC, however, the window disappears before the final output can be seen. Hence this routine requests the user to perform the final "halt." */
 finish :-
     nl,
-    write('The game is over. Please enter the "halt." command.'),
+    write('Game Over! Du hast nichts mehr zu essen. Bitte schreibe den command "halt." um danach noch mal zu spielen'),
     nl.
 
 /* This rule shows what you have in your inventory */
@@ -168,8 +169,8 @@ food_timer :-
         NewFood is Food - 1,
         retract(food(Food)),
         assert(food(NewFood)),
-        format('Du hast noch ~w Burger übrig.~n', [NewFood]),
-        sleep(30),  % More time until a burger is consumed
+        format('Du hast noch ~w Burger uebrig.~n', [NewFood]),
+        sleep(80),  % More time until a burger is consumed
         food_timer
     ;
         die
@@ -177,38 +178,47 @@ food_timer :-
 
 /* These rules describe the various rooms. Depending on circumstances, a room may have more than one description. */
 describe(home) :- 
-    write('Du bist in deinem Haus. Du hast nicht lange Zeit, um dich zu entscheiden, welche Gegenstände du mit in deinen Bunker nimmst, bevor eine Atombombe einschlägt und die Oberwelt verseucht wird.'), nl, 
-    write('Vor dir Richtung Osten liegt die Treppe, welche in den Bunker führt.'), nl.
+    write('Du bist in deinem Haus. Du hast nicht lange Zeit, um dich zu entscheiden, welche Gegenstaende du mit in deinen Bunker nimmst, bevor eine Atombombe einschlaegt und die Oberwelt verseucht wird.'), nl, 
+    write('Vor dir Richtung Osten liegt die Treppe, welche in den Bunker fuehrt.'), nl.
 
 describe(bunkerdoor) :-
-    write('Vor dir liegt eine Tür, die in den Bunker führt.'), nl.
+    write('Vor dir liegt eine Tuer, die in den Bunker fuehrt.'), nl.
 
 describe(bunker) :-
-    write('Du betrittst deinen Bunker. Es ist dunkel, aber du kannst dennoch die Umrisse der Objekte erkennen. Da du jetzt im Bunker bist, musst du einen Weg finden, um Hilfe von der Außenwelt anzufragen. Aber dafür hast du nicht lange Zeit, da du nur begrenzt Essen hast.'), nl,
-    write('Einige davon könnten nützliche Gegenstände enthalten. Vielleicht solltest du sie durchsuchen.'), nl.
+    write('Du betrittst deinen Bunker. Es ist dunkel, aber du kannst dennoch die Umrisse der Objekte erkennen. Da du jetzt im Bunker bist, musst du einen Weg finden, um Hilfe von der Außenwelt anzufragen. Aber dafuer hast du nicht lange Zeit, da du nur begrenzt Essen hast.'), nl,
+    write('Einige davon koennten nuetzliche Gegenstaende enthalten. Vielleicht solltest du sie durchsuchen.'), nl.
 
 describe(livingroom) :-
-    write('Du befindest dich im Wohnzimmer deines Bunkers. Hier kannst du dich ausruhen und entspannen, wenn die Welt da draußen zerstört ist.'), nl,
-    write('Es gibt einen Fernseher an der Wand und eine Tür im Osten, die zum Korridor führt.'), nl.
+    write('Du befindest dich im Wohnzimmer deines Bunkers. Hier kannst du dich ausruhen und entspannen, wenn die Welt da draußen zerstoert ist.'), nl,
+    write('Es gibt einen Fernseher an der Wand und eine Tuer im Osten, die zum Korridor fuehrt.'), nl.
 
 describe(tv) :-
     write('Der Fernseher zeigt nur statisches Rauschen. Kein Signal von draußen.'), nl.
 
 describe(corridor1) :-
-    write('Du befindest dich im Korridor deines Bunkers. Ein paar Türen führen zu verschiedenen Räumen.'), nl,
-    write('Im Osten führt ein Korridor weiter. Im Süden liegt ein Schlafzimmer.'), nl.
+    write('Du befindest dich im Korridor deines Bunkers. Ein paar Tueren fuehren zu verschiedenen Raeumen.'), nl,
+    write('Im Osten fuehrt ein Korridor weiter. Im Sueden liegt ein Schlafzimmer.'), nl.
 
 describe(corridor2) :-
-    write('Dieser Korridor ist etwas breiter als der letzte. An der Wand hängen einige Regale mit Vorräten.'), nl,
+    write('Dieser Korridor ist etwas breiter als der letzte. An der Wand haengen einige Regale mit Vorraeten.'), nl,
     write('Ein Regal scheint locker zu sein. Vielleicht kannst du es untersuchen.'), nl.
 
 describe(storage) :-
-    write('Du betrittst den Lagerraum. Hier werden die lebenswichtigen Vorräte für den Überlebenskampf gelagert.'), nl,
+    write('Du betrittst den Lagerraum. Hier werden die lebenswichtigen Vorraete fuer den Ueberlebenskampf gelagert.'), nl,
     write('Du siehst Konserven, Wasserflaschen und Medikamente auf den Regalen.'), nl.
 
 describe(bedroom) :-
-    write('Du betrittst das Schlafzimmer. Es ist einfach, aber gemütlich. Ein Ort zum Ausruhen und Schlafen.'), nl,
+    write('Du betrittst das Schlafzimmer. Es ist einfach, aber gemuetlich. Ein Ort zum Ausruhen und Schlafen.'), nl,
     write('Es gibt ein Bett und einen Schrank.'), nl.
 
+describe(workbench) :-
+    write('Vor dir liegt eine Werkbank vielleicht kannst du ja etwas herstellen.'), nl,
+    write('Du koenntest zum Beispiel einen Dietrich bauen.'), nl.
+
+describe(schrank) :-
+    write('Vor dir befindet sich ein Schrank du koenntest ihn durchsuchen und etwas nuetzliches finden.'), nl.
+
+describe(vent) :-
+    write('Ueber dir befindet sich ein Lueftungsschacht. Du koenntest ihn wahrscheinlich mit einem Schraubenzieher oeffnen.'), nl.
 
 /* Prepare Phase*/
